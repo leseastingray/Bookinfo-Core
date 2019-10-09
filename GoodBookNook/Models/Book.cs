55fc2234d1509f3be127c4d60cd2b081d31960c8
leseastingray <leseastingray@my.lanecc.edu>
2019-10-09 10:40:33 -0700

    test

--- GoodBookNook/Models/Book.cs
+++ GoodBookNook/Models/Book.cs
@@ -10,9 +10,16 @@ namespace GoodBookNook.Models
         private List<Author> authors = new List<Author>();
         private List<Review> reviews = new List<Review>();
 
-        [StringLength(100, MinimumLength = 2)]  
+        [StringLength(100, MinimumLength = 2)]
         [Required]
-        public string Title { get; set; }
+        //public string Title { get; set; }
+        // below is a full fledged version of the above automatic property:
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
         public DateTime PubDate { get; set; }
 
         // ICollection is more flexible than List and can be modified. IEnumerable can't be modified
